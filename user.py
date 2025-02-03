2d05a78440d35ef44a2a32cb66f8d5d362b58502
Joonatan Paijo <156064167+50904@users.noreply.github.com>
2025-02-03 09:34:49 +0200

    muutokset ja ylimääräisten poisto 3.2

--- user.py
+++ user.py
@@ -6,7 +6,7 @@
 # ----------------------------------
 import os # Polkumääritykset
 import sys # Käynnistysargumentit
-import json # Json-tiedostojen käsittely
+import json # JSON-tiedostojen käsittely
 
 from PySide6 import QtWidgets # Qt-vimpaimet
 
@@ -14,27 +14,25 @@ from lendingModules import sound # Äänitoiminnot
 from lendingModules import dbOperations # Tietokantatoiminnot
 from lendingModules import cipher # Salausmoduuli
 
-# Tuodaan käyttöliittymään Pythoniksi käännetty tiedosto 
-# Korvaa mainwindow_ui todellisella tiedoston nimellä
+# mainWindow_ui:n tilalle käännetyn pääikkunan tiedoston nimi
+# ilman .py-tiedostopäätettä
 from user_ui import Ui_MainWindow # Käännetyn käyttöliittymän luokka
 
-# Määritellään luokka joka perii QMainWindow- ja Ui_
+# Määritellään luokka, joka perii QMainWindow- ja Ui_MainWindow-luokan
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
-    """A class for creating main window for the application"""
-
-    # Määritellään oliomuodostin ja kutsutaan yliluokkien muodostimia
+    """A class for creating a main window for the application"""
+    
+    # Määritellään olionmuodostin ja kutsutaan yliluokkien muodostimia
     def __init__(self):
         super().__init__()
 
-
-        # Luodaan käyttöliittymä konvertoidun tiedoston perusteella MainWindown:n ui-ominaisuudeksi.
-        # Tämä suojaa lopun MainWindow-olion ylikirjoitukselta, kun ui-tiedostoa päivitetään
+        # Luodaan käyttöliittymä konvertoidun tiedoston perusteella MainWindow:n ui-ominaisuudeksi. Tämä suojaa lopun MainWindow-olion ylikirjoitukselta, kun ui-tiedostoa päivitetään
         self.ui = Ui_MainWindow()
 
         # Kutsutaan käyttöliittymän muodostusmetodia setupUi
         self.ui.setupUi(self)
 
-                # Rutiini, joka lukee asetukset, jos ne ovat olemassa
+        # Rutiini, joka lukee asetukset, jos ne ovat olemassa
         try:
             # Avataam asetustiedosto ja muutetaan se Python sanakirjaksi
             with open('settings.json', 'rt') as settingsFile: # With sulkee tiedoston automaattisesti
@@ -44,148 +42,171 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             
             # Puretaan salasana tietokantaoperaatioita varten  
             self.plainTextPassword = cipher.decryptString(self.currentSettings['password'])
+        
         except Exception as error:
             self.openWarning()
 
-        # Äänet oletuksena käytössä
-        self.soundOnPictureLabel = True
 
+        # Äänet oletuksena käytössä
+        self.soundOn = True
 
         # Ohjelman käynnistyksessä piilotetaan tarpeettomat elementit
-        self.setInitialElements(self)
+        self.setInitialElements()
 
 
         # OHJELMOIDUT SIGNAALIT
         # ---------------------
-
-        # Palauta käyttöliittymä alkutilanteeseen
-    def setInitialElements(self):
-        self.ui.returnCarPushButton.show()
-        self.ui.borrowCarPushButton.show()
-        self.ui.calendarPictureLabel.hide()
-        self.ui.clockTimeLabel.hide()
-        self.ui.dateTimeLabel.hide()
-        self.ui.goBackPictureLabel.hide()
-        self.ui.readyKeyLineEdit.hide()
-        self.ui.keyPicturesLabel.hide()
-        self.ui.readLicenceLineEdit.hide()
-        self.ui.keyBarcodeLineEdit.clear()
-        self.ui.keyBarcodeLineEdit.hide()
-        self.ui.soundOnPictureLabel.hide()
-        self.ui.ssnLineEdit.clear()
-        self.ui.ssnLineEdit.hide()
-        self.ui.statusLabel.hide()
-        self.ui.dateTimeLabel.hide()
-        self.ui.lenderNameLabel.hide()
-        self.ui.carinfoLabel.hide()
-
-        # Kun Lainaa-painiketta painetaan, kutsutaan activateLender metodia 
+        
+        # Kun Lainaa-painiketta painetaan, kutsutaan activateLender-metodia
         self.ui.takeCarPushButton.clicked.connect(self.activateLender)
 
         # Kun ajokortin viivakoodi on luettu, kutsutaan activateKey-metodia
         self.ui.ssnLineEdit.returnPressed.connect(self.activateKey)
-
-        # Kun avaimenperä on luettu, kutsutaan setLendingData
+        
+        # Kun avaimenperä on luettu, kutsutaan setLendingData-metodia
         self.ui.keyBarcodeLineEdit.returnPressed.connect(self.setLendingData)
 
-        # Kun OK-painiketta on painettu, tallenna tiedot ja palauta käyttöliittymä alkutilaan
+        # Kun OK-painiketta on painettu, tallenna tiedot 
+        # ja palauta käyttöliittymä alkutilaan
         self.ui.okPushButton.clicked.connect(self.saveLendingData)
-        
+
         # Kun palauta-painiketta on painettu, kutsutaan activateReturnCar-metodia
         self.ui.returnCarPushButton.clicked.connect(self.activateReturnCar)
 
-        # Kun avaimenperä on luettu palautettaessa, kutsutaan saveReturnData-metodia
+        # Kun avaimenperä on luettu palutettaessa, kutsutaan saveReturnData-metodia
         self.ui.keyReturnBarcodeLineEdit.returnPressed.connect(self.saveReturnData)
+    
+        # Kun mykistä painiketta painetaan, kutsutaan mute-metodia
+        self.ui.soundOffPushButton.clicked.connect(self.mute)
 
-        # Kun mykistä painkietta painetaan kutsutaan mute-metodia
-        self.ui.soundOffPictureLabel.clicked.connect(self.mute)
-
-        # Kun äänipainiketta painetaan kutsutaan unmute-metodia
-        self.ui.soundOnPictureLabel.clicked.connect(self.unmute)
+        # Kun äänipäiniketta painetaan, kutsutaan unmute-metodia
+        self.ui.soundOnPushButton.clicked.connect(self.unmute)
 
         # Kun kumoa painiketta painetaan palautetaan UI-alkutilaan
-        self.ui.goBackpushButton.clicked.connect(self.goBack)
-
+        self.ui.goBackPushButton.clicked.connect(self.goBack)
+    
     # OHJELMOIDUT SLOTIT
     # ------------------
+
+    # Palauta käyttöliittymä alkutilanteeseen
+    def setInitialElements(self):
+        self.ui.returnCarPushButton.show()
+        self.ui.takeCarPushButton.show()
+        self.ui.okPushButton.hide()
+        self.ui.calendarLabel.hide()
+        self.ui.clockLabel.hide()
+        self.ui.dateLabel.hide()
+        self.ui.goBackPushButton.hide()
+        self.ui.keyBarcodeLineEdit.clear()
+        self.ui.keyBarcodeLineEdit.hide()
+        self.ui.keyReturnBarcodeLineEdit.clear()
+        self.ui.keyReturnBarcodeLineEdit.hide()
+        self.ui.keyPictureLabel.hide()
+        self.ui.lenderPictureLabel.hide()
+        self.ui.soundOnPushButton.hide()
+        self.ui.ssnLineEdit.clear()
+        self.ui.ssnLineEdit.hide()
+        self.ui.statusLabel.hide()
+        self.ui.timeLabel.hide()
+        self.ui.lenderNameLabel.hide()
+        self.ui.carInfoLabel.hide()
 
     # Näyttää lainaajan kuvakkeen ja henkilötunnuksen kentän
     def activateLender(self):
         self.ui.statusLabel.setText('Auton lainaus')
         self.ui.lenderPictureLabel.show()
         self.ui.ssnLineEdit.show()
-        self.ui.goBackPictureLabel.show()
+        self.ui.goBackPushButton.show()
         self.ui.ssnLineEdit.setFocus()
         self.ui.returnCarPushButton.hide()
-        self.ui.borrowCarPushButton.hide()
+        self.ui.takeCarPushButton.hide()
         self.ui.statusLabel.show()
         self.ui.statusbar.showMessage('Syötä ajokortti koneeseen')
         if self.soundOn:
-            sound.playWAV('Sounds\\drivingLicence.WAV')
-
+            sound.playWav('sounds\\drivingLicence.WAV')
+        
 
     # Näyttää avaimen kuvakkeen, rekisterikenttä ja lainaajan tiedot
     def activateKey(self):
-        self.ui.keyPicturesLabel.show()
+        self.ui.keyPictureLabel.show()
         self.ui.keyBarcodeLineEdit.show()
         self.ui.keyBarcodeLineEdit.setFocus()
         self.ui.lenderNameLabel.show()
+        self.ui.statusbar.showMessage('Syötä avaimenperä koneeseen')
+        if self.soundOn:
+            sound.playWav('sounds\\readKey.WAV')
 
     # Näyttää lainauksen loput tiedot
     def setLendingData(self):
-        self.ui.carinfoLabel.show()
-        self.ui.dateTimeLabel.show()
-        self.ui.clockTimeLabel.show()
+        self.ui.carInfoLabel.show()
+        self.ui.dateLabel.show()
+        self.ui.calendarLabel.show()
+        self.ui.timeLabel.show()
+        self.ui.clockLabel.show()
         self.ui.okPushButton.show()
+        self.ui.statusbar.showMessage('Jos tiedot ovat oikein paina OK')
+        if self.soundOn:
+            sound.playWav('sounds\\saveData.WAV')
 
     # Tallennetaan lainauksen tiedot ja palautetaan käyttöliittymä alkutilaan
     def saveLendingData(self):
         # Save data to the database
+        # Luetaan tietokanta-asetukset paikallisiin muuttujiin
+        dbSettings = self.currentSettings
+        plainTextPassword = self.plainTextPassword
+        dbSettings['password'] = plainTextPassword # Vaidetaan selväkieliseksi
+
+        # TODO: Laita seuraava lohko virheenkäsittelyn sisälle
+        # Luodaan tietokantayhteys-olio
+        dbConnection = dbOperations.DbConnection(dbSettings)
         ssn = self.ui.ssnLineEdit.text()
         key = self.ui.keyBarcodeLineEdit.text()
-        dbOperations.DbConnection()
+        dataDictionary = {'hetu': ssn,
+                          'rekisterinumero': key}
+        dbConnection.addToTable('lainaus', dataDictionary)
+
         self.setInitialElements()
         self.ui.statusbar.showMessage('Auton lainaustiedot tallennettiin', 5000)
-        
-    
-    # Näytetään palautukseen liittyvät kentät ja kuvat 
+        if self.soundOn:
+            sound.playWav('sounds\\lendingOk.WAV')
+
+    # Näytetään palautukseen liittyvät kentät ja kuvat
     def activateReturnCar(self):
-        self.ui.borrowCarPushButton.hide()
-        self.ui.returnCarPushButton.show()
-        self.ui.keyPicturesLabel.show()
+        self.ui.takeCarPushButton.hide()
+        self.ui.returnCarPushButton.hide()
+        self.ui.statusLabel.setText('Auton palautus')
+        self.ui.keyPictureLabel.show()
         self.ui.keyReturnBarcodeLineEdit.show()
         self.ui.keyReturnBarcodeLineEdit.setFocus()
         self.ui.statusbar.showMessage('Lue avaimen viivakoodi')
         if self.soundOn:
-            sound.playWav('sounds\\lendingOK.Wav')
+            sound.playWav('sounds\\readKey.WAV')
 
-    # TODO: Laita seuraava lohko virheenkäsittelyn sisälle
-    # Luodaan tietokantayhteys olioon
-
-    # Tallennetaan palautuksen tiedot tietokantaan ja palautetaan UI alkutila
+    # Tallennetaan palautuksen tiedot tietokantaan ja palautetaan UI alkutilaan
     def saveReturnData(self):
         self.ui.statusbar.showMessage('Auto palautettu')
         self.setInitialElements()
-    
+        if self.soundOn:
+            sound.playWav('sounds\\returnOk.WAV')
+
     # Mykistetään äänet
     def mute(self):
-        self.soundOffPictureLabel.hide()
-        self.ui.soundOnPictureLabel.show()
+        self.ui.soundOffPushButton.hide()
+        self.ui.soundOnPushButton.show()
         self.ui.statusbar.showMessage('Äänet mykistetty')
-        self.sounfOn = False
+        self.soundOn = False
     
     # Poistetaan mykistys
     def unmute(self):
-        self.soundOffPictureLabel.show()
-        self.ui.statusbar.showMessage('Äänet päällä')
-        self.ui.soundOnPictureLabel.hide()
-        self.sounfOn = True
-
+        self.ui.soundOffPushButton.show()
+        self.ui.soundOnPushButton.hide()
+        self.ui.statusbar.showMessage('Äänet käytössä')
+        self.soundOn = True
+    
     def goBack(self):
         self.setInitialElements()
         self.ui.statusbar.showMessage('Toiminto peruutettiin', 5000)
-
-
+        
     # Avataan MessageBox
     def openWarning(self):
         msgBox = QtWidgets.QMessageBox()
@@ -194,6 +215,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         msgBox.setText('Ota yhteyttä järjestelmän valvojaan')
         msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msgBox.exec()
+
 
 # LUODAAN VARSINAINEN SOVELLUS
 # ============================
@@ -206,3 +228,4 @@ window.show()
 # Käynnistetään sovellus ja tapahtumienkäsittelijä (event loop)
 app.exec()
 
+    
